8e6747cee059253ab506d970250d7f102acc0633
davidsibato <50614879+davidsibato@users.noreply.github.com>
2020-11-17 12:48:54 +0200

    added secured https

--- js/api.js
+++ js/api.js
@@ -5,9 +5,9 @@ class OMDB {
     }
  
     async getMovies(){
-        const movieResponse= await fetch(`http://www.omdbapi.com/?apikey=${this.key}&s=movie
+        const movieResponse= await fetch(`https://www.omdbapi.com/?apikey=${this.key}&s=movie
         `);
-        const seriesResponse= await fetch(`http://www.omdbapi.com/?apikey=${this.key}&s=new
+        const seriesResponse= await fetch(`https://www.omdbapi.com/?apikey=${this.key}&s=new
         `);
 
         const movieResults= await movieResponse.json();
@@ -20,14 +20,14 @@ class OMDB {
 
     async movieInfo(id){
         
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${this.key}&i=${id}
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${this.key}&i=${id}
         `);
         const results =  await response.json();
         return results;
     }
 
     async search(userText){
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${this.key}&s=${userText}
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${this.key}&s=${userText}
         `);
         const results =  await response.json();
         
